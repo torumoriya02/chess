753749a06b5468548056716a101c3439c2741076
torumoriya02 <toru9149@gmail.comm>
2026-07-15 17:56:41 -0600

    make dataaccess

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,30 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+public interface DataAccess {
+
+    void clear() throws DataAccessException;
+
+    void createUser(UserData user) throws DataAccessException;
+
+    UserData getUser(String username) throws DataAccessException;
+
+    void createAuth(AuthData auth) throws DataAccessException;
+
+    AuthData getAuth(String authToken) throws DataAccessException;
+
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    int createGame(GameData game) throws DataAccessException;
+
+    GameData getGame(int gameID) throws DataAccessException;
+
+    Collection<GameData> listGames() throws DataAccessException;
+
+    void updateGame(GameData game) throws DataAccessException;
+}
