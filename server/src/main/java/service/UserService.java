492c29aee4869acdd7e0120da6f11bef138de2a1
torumoriya02 <toru9149@gmail.comm>
2026-07-15 18:26:32 -0600

    UserService2

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,6 +1,11 @@
 package service;
 
 import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.AuthData;
+import model.UserData;
+
+import java.util.UUID;
 
 public class UserService {
 
@@ -8,5 +13,47 @@ public class UserService {
 
     public UserService(DataAccess dataAccess) {
         this.dataAccess = dataAccess;
+    }
+
+    public RegisterResult register(RegisterRequest request)
+            throws DataAccessException {
+
+        if (request == null
+                || request.username() == null
+                || request.password() == null
+                || request.email() == null
+                || request.username().isBlank()
+                || request.password().isBlank()
+                || request.email().isBlank()) {
+            throw new IllegalArgumentException("Error: bad request");
+        }
+
+        UserData existingUser = dataAccess.getUser(request.username());
+
+        if (existingUser != null) {
+            throw new IllegalStateException("Error: already taken");
+        }
+
+        UserData newUser = new UserData(
+                request.username(),
+                request.password(),
+                request.email()
+        );
+
+        dataAccess.createUser(newUser);
+
+        String authToken = UUID.randomUUID().toString();
+
+        AuthData authData = new AuthData(
+                authToken,
+                request.username()
+        );
+
+        dataAccess.createAuth(authData);
+
+        return new RegisterResult(
+                request.username(),
+                authToken
+        );
     }
 }
