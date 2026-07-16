7098c5e32f0abfc44018ac762dd5ae8c1ed67cbd
torumoriya02 <toru9149@gmail.comm>
2026-07-16 09:23:45 -0600

    new server

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,11 +3,16 @@ package server;
 import dataaccess.MemoryDataAccess;
 import io.javalin.Javalin;
 import service.ClearService;
+import com.google.gson.Gson;
+import service.RegisterRequest;
+import service.UserService;
 
 public class Server {
 
     private final Javalin javalin;
     private final MemoryDataAccess dataAccess = new MemoryDataAccess();
+    private final UserService userService = new UserService(dataAccess);
+    private final Gson gson = new Gson();
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
@@ -18,6 +23,17 @@ public class Server {
 
             ctx.status(200);
             ctx.result("{}");
+        });
+
+        javalin.post("/user", ctx -> {
+            RegisterRequest request =
+                    gson.fromJson(ctx.body(), RegisterRequest.class);
+
+            var result = userService.register(request);
+
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(gson.toJson(result));
         });
     }
 
