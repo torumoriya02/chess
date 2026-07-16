603d61e873a71061fb4d6f380bfd5493aa63788f
torumoriya02 <toru9149@gmail.comm>
2026-07-16 10:42:40 -0600

    Game post

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -7,6 +7,8 @@ import com.google.gson.Gson;
 import service.RegisterRequest;
 import service.UserService;
 import service.LoginRequest;
+import service.GameService;
+import service.CreateGameRequest;
 
 public class Server {
 
@@ -14,6 +16,7 @@ public class Server {
     private final MemoryDataAccess dataAccess = new MemoryDataAccess();
     private final UserService userService = new UserService(dataAccess);
     private final Gson gson = new Gson();
+    private final GameService gameService = new GameService(dataAccess);
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
@@ -38,14 +41,49 @@ public class Server {
         });
 
         javalin.post("/session", ctx -> {
-        LoginRequest request =
-                gson.fromJson(ctx.body(), LoginRequest.class);
+            LoginRequest request =
+                    gson.fromJson(ctx.body(), LoginRequest.class);
 
-        var result = userService.login(request);
+            var result = userService.login(request);
 
-        ctx.status(200);
-        ctx.contentType("application/json");
-        ctx.result(gson.toJson(result));
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(gson.toJson(result));
+        });
+
+        javalin.delete("/session", ctx -> {
+            String authToken = ctx.header("authorization");
+
+            userService.logout(authToken);
+
+            ctx.status(200);
+            ctx.result("{}");
+        });
+
+        javalin.get("/game", ctx -> {
+            String authToken = ctx.header("authorization");
+
+            System.out.println("GET /game token: [" + authToken + "]");
+            System.out.println("Stored auth: " + dataAccess.getAuth(authToken));
+
+            var result = gameService.listGames(authToken);
+
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(gson.toJson(result));
+        });
+
+        javalin.post("/game", ctx -> {
+            String authToken = ctx.header("authorization");
+
+            CreateGameRequest request =
+                    gson.fromJson(ctx.body(), CreateGameRequest.class);
+
+            var result = gameService.createGame(authToken, request);
+
+            ctx.status(200);
+            ctx.contentType("application/json");
+            ctx.result(gson.toJson(result));
         });
     }
 
