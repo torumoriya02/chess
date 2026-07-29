4289a448c6b62d35b18a731282cb8b102cdd6228
torumoriya02 <toru9149@gmail.comm>
2026-07-28 19:16:06 -0600

    Add prelogin

---
+++ client/src/main/java/client/ChessClient.java
@@ -0,0 +1,11 @@
+package client;
+
+public class ChessClient {
+
+    public ChessClient(String serverUrl) {
+    }
+
+    public void run() {
+        System.out.println("Chess client started.");
+    }
+}
