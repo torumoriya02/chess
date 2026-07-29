a540b32c3d4f72f033dd47ced73287b3eea3f81c
torumoriya02 <toru9149@gmail.comm>
2026-07-29 10:18:24 -0600

    Register command to client

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -7,8 +7,12 @@ public class ChessClient {
     private final String serverUrl;
     private final Scanner scanner = new Scanner(System.in);
 
+    private final ServerFacade facade;
+    private String authToken;
+
     public ChessClient(String serverUrl) {
         this.serverUrl = serverUrl;
+        this.facade = new ServerFacade(serverUrl);
     }
 
     public void run() {
@@ -26,6 +30,8 @@ public class ChessClient {
 
             if (input.equalsIgnoreCase("help")) {
                 printPreloginHelp();
+            } else if (input.equalsIgnoreCase("register")) {
+                register();
             } else {
                 System.out.println("Unknown command. Type help.");
             }
@@ -37,5 +43,43 @@ public class ChessClient {
         System.out.println("login    - log in to your account");
         System.out.println("register - create a new account");
         System.out.println("quit     - exit the program");
+    }
+
+    private void register() {
+        try {
+            System.out.print("Username: ");
+            String username = scanner.nextLine().trim();
+
+            System.out.print("Password: ");
+            String password = scanner.nextLine().trim();
+
+            System.out.print("Email: ");
+            String email = scanner.nextLine().trim();
+
+            var result = facade.register(username, password, email);
+            authToken = result.authToken();
+
+            System.out.println("Registered and logged in as " + result.username());
+
+        } catch (Exception ex) {
+            System.out.println("Unable to register. Please check your information.");
+        }
+    }
+    private void login() {
+        try {
+            System.out.print("Username: ");
+            String username = scanner.nextLine().trim();
+
+            System.out.print("Password: ");
+            String password = scanner.nextLine().trim();
+
+            var result = facade.login(username, password);
+            authToken = result.authToken();
+
+            System.out.println("Logged in as " + result.username());
+
+        } catch (Exception ex) {
+            System.out.println("Unable to log in. Check your username and password.");
+        }
     }
 }
